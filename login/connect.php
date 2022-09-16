16df95f9ff4658762198dba05048284f2c831ed0
Dihan141 <87206556+Dihan141@users.noreply.github.com>
2022-09-16 18:38:46 +0600

    updated connect.php
    given password will be checked with confirmation password, then register into database

--- login/connect.php
+++ login/connect.php
@@ -3,10 +3,12 @@
     $username = '';
     $email = '';
     $password = '';
+    $confirmPass = '';
     if(isset($_POST['submit'])){
         $username=$_POST['username'];
         $email=$_POST['email'];
         $password=$_POST['password'];
+        $confirmPass = $_POST['confirmPass'];
     }
 
     $conn = new mysqli('localhost','root','','spl');
@@ -17,13 +19,22 @@
     else{
         // $SQL="INSERT INTO `users`(`username`, `email`, `password`) VALUES ('$username','$email','$password')";
         // $result=mysqli_query($conn,$SQL);
-        $stmt = $conn->prepare("insert into users(username,email,password) values(?,?,?)");
-        $stmt ->bind_param("sss",$username, $email, $password);
-        $stmt -> execute();
-        header('location: ../Homepage/index.html');
-        $stmt -> close();
+        
+        //get 2nd password from index and compare with the first
+        if($password === $confirmPass)
+        {
+            $stmt = $conn->prepare("insert into users(username,email,password) values(?,?,?)");
+            $stmt ->bind_param("sss",$username, $email, $password);
+            $stmt -> execute();
+            header('location: ../Homepage/index.html');
+            $stmt -> close();
+        }
+        else
+        {
+            echo "Password doesn't match. Try again.";
+        }
         $conn -> close();
-    }
+    } 
 
 
 ?>
